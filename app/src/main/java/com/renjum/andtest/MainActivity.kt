5d1dcbd91b5253304348da7bf8f27d964c064258
Renju Mathew <honourister@outlook.com>
2019-03-23 16:43:06 +0530

    Devslopes

--- app/src/main/java/com/renjum/andtest/MainActivity.kt
+++ app/src/main/java/com/renjum/andtest/MainActivity.kt
@@ -1,14 +1,23 @@
 package com.renjum.andtest
 
+import android.graphics.BitmapFactory
+import android.graphics.drawable.BitmapDrawable
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.graphics.drawable.RoundedBitmapDrawableFactory
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlin.math.round
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        var occupation = "developer"
-        var name = "renjum"
+
+        val bitmap = BitmapFactory.decodeResource(resources, R.drawable.devslopesprofilelogo)
+        val rounded = RoundedBitmapDrawableFactory.create(resources, bitmap)
+        rounded.cornerRadius = 15f
+        logo.setImageDrawable(rounded)
+
     }
 }
